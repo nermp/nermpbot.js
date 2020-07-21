898ba021e99693e0b0eaa5a9d2347b1a0525ba24
nermp <edwintan7962@gmail.com>
2020-07-21 13:56:19 -0500

    port listening test

---
+++ express.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => res.send('Ok'));
+app.listen(process.env.PORT, () => console.log("Your app is listening on port " + process.env.PORT));
