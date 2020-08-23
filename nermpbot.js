5bd1ce0faddfe07f4416bd55a7ed080b5e8b7de3
nermp <edwintan7962@gmail.com>
2020-08-23 15:27:34 -0500

    test for removing web dyno

--- nermpbot.js
+++ nermpbot.js
@@ -7,6 +7,11 @@ module.exports = {
 // require the necessary modules
 const fs = require("fs");
 const Discord = require("discord.js");
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => res.send("nermpbot is currently operational"));
+app.listen(process.env.PORT, () => console.log("nermpbot is listening on port " + process.env.PORT));
 
 // define constants and variables
 const { prefix, numberOfServers } = require("./config.json");
